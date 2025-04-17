6819529b31c66c43e75eb0810249e9c1de460bb4
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-17 18:59:14 -0400

    make AppBundles

--- desmume-appimage.sh
+++ desmume-appimage.sh
@@ -56,6 +56,16 @@ echo "Generating AppImage..."
 	--header uruntime \
 	-i ./AppDir -o "$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
 
+# Set up the PELF toolchain
+wget -qO ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$(uname -m)" && chmod +x ./pelf
+echo "Generating [dwfs]AppBundle...(Go runtime)"
+./pelf --add-appdir ./AppDir \
+	--appbundle-id="${PACKAGE}-${VERSION}" \
+	--compression "-C zstd:level=22 -S26 -B8" \
+	--output-to "${PACKAGE}-${VERSION}-anylinux-${ARCH}.dwfs.AppBundle"
+
 echo "Generating zsync file..."
 zsyncmake *.AppImage -u *.AppImage
+zsyncmake *.AppBundle -u *.AppBundle
+
 echo "All Done!"
