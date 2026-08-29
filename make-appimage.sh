b4c5adcb705cdad788dca106c2c710b34bf4b4f7
Samueru-sama <xdglawyer@outlook.com>
2026-08-28 20:35:28 -0400

    test in prod

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,9 +13,14 @@ export DESKTOP=/usr/share/applications/org.desmume.DeSmuME.desktop
 export APPNAME=DeSmuME
 export DEPLOY_SDL=1
 export DEPLOY_OPENGL=1
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/desmume
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
